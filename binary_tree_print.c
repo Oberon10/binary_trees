f7a34eb44d63432c01910eb8f1b454dc331e3145
oberon10 <prosperbernrad@gmail.com>
2023-09-01 19:41:25 -0700

    task completed

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -3,15 +3,18 @@
 #include <string.h>
 #include "binary_trees.h"
 
+/* Original code from http://stackoverflow.com/a/13755911/5184480 */
+
 /**
  * print_t - Stores recursively each level in an array of strings
+ *
  * @tree: Pointer to the node to print
  * @offset: Offset to print
  * @depth: Depth of the node
  * @s: Buffer
+ *
  * Return: length of printed tree after process
  */
-
 static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
 {
 	char b[6];
@@ -42,10 +45,11 @@ static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
 
 /**
  * _height - Measures the height of a binary tree
+ *
  * @tree: Pointer to the node to measures the height
+ *
  * Return: The height of the tree starting at @node
  */
-
 static size_t _height(const binary_tree_t *tree)
 {
 	size_t height_l;
@@ -58,9 +62,9 @@ static size_t _height(const binary_tree_t *tree)
 
 /**
  * binary_tree_print - Prints a binary tree
+ *
  * @tree: Pointer to the root node of the tree to print
  */
-
 void binary_tree_print(const binary_tree_t *tree)
 {
 	char **s;
--- tests/102-main.c
+++ tests/102-main.c
@@ -1,16 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "../binary_trees.h"
-
-/**
- * print_num - Prints a number
- *
- * @n: Number to be printed
- */
-void print_num(int n)
-{
-    printf("%d\n", n);
-}
+#include "binary_trees.h"
 
 /**
  * main - Entry point
@@ -20,17 +10,36 @@ void print_num(int n)
 int main(void)
 {
     binary_tree_t *root;
+    int complete;
 
     root = binary_tree_node(NULL, 98);
     root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    root->left->left = binary_tree_node(root->left, 6);
-    root->left->right = binary_tree_node(root->left, 56);
-    root->right->left = binary_tree_node(root->right, 256);
-    root->right->right = binary_tree_node(root->right, 512);
+    root->right = binary_tree_node(root, 128);
+    root->left->right = binary_tree_node(root->left, 54);
+    root->right->right = binary_tree_node(root, 402);
+    root->left->left = binary_tree_node(root->left, 10);
 
     binary_tree_print(root);
-    binary_tree_levelorder(root, &print_num);
+    complete = binary_tree_is_complete(root);
+    printf("Is %d complete: %d\n", root->n, complete);
+    complete = binary_tree_is_complete(root->left);
+    printf("Is %d complete: %d\n", root->left->n, complete);
+
+    root->right->left = binary_tree_node(root->right, 112);
+    binary_tree_print(root);
+    complete = binary_tree_is_complete(root);
+    printf("Is %d complete: %d\n", root->n, complete);
+
+    root->left->left->left = binary_tree_node(root->left->left, 8);
+    binary_tree_print(root);
+    complete = binary_tree_is_complete(root);
+    printf("Is %d complete: %d\n", root->n, complete);
+
+    root->left->right->left = binary_tree_node(root->left->right, 23);
+    binary_tree_print(root);
+    complete = binary_tree_is_complete(root);
+    printf("Is %d complete: %d\n", root->n, complete);
+
     binary_tree_delete(root);
     return (0);
 }
